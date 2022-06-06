bb9d104000f008e4e8c116ddb63c635f9ca529c6
Carlos Escriche <escrichov@gmail.com>
2022-06-06 16:01:48 +0200

    Added host

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n=9u%-2!+(m0nj$%tu510+tv(1@8y9v2c*%1m4*^209le3^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*.pythonapp.io']
+ALLOWED_HOSTS = ['test.pythonapp.io']
 
 
 # Application definition
